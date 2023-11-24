be54e2ab924cc09ec443655a5985cc917b846980
Jorge <jortuone@gmail.com>
2023-11-23 19:12:59 -0700

    Cambios 7:12 PM 23/11/2023

--- src/components/HomePublicaciones.vue
+++ src/components/HomePublicaciones.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="grid-container">
-    <div class="grid-item" v-for="news in dummyNews" :key="news.id">
-      <img :src="news.image" alt="News Image" class="news-image" />
-      <h3 class="news-title">{{ news.title }}</h3>
-      <p class="news-description">{{ news.description }}</p>
+  <div class="news-grid">
+    <div v-for="news in newsList" :key="news.id" class="news-card">
+      <h2>{{ news.title }}</h2>
+      <img :src="news.image" alt="News Image" class="news-image">
+      <p>{{ news.content }}</p>
+      <div class="comment-bar">
+        <input type="text" v-model="commentInput" placeholder="Add a comment">
+        <button @click="addComment(news.id)">Add Comment</button>
+      </div>
+      <div class="comments">
+        <div v-for="comment in news.comments" :key="comment.id" class="comment">
+          <p>{{ comment.text }}</p>
+        </div>
+      </div>
+      <button @click="likeNews(news.id)">
+        {{ news.likes }} Likes
+      </button>
     </div>
   </div>
 </template>
@@ -12,64 +24,100 @@
 export default {
   data() {
     return {
-      dummyNews: [
+      newsList: [
         {
           id: 1,
-          title: "Dummy News 1",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          image: require("../assets/Noticia1.jpg")
+          title: "News 1",
+          content: "This is the content of news 1",
+          image: require("../assets/Noticia1.jpg"),
+          comments: [],
+          likes: 0
         },
         {
           id: 2,
-          title: "Dummy News 2",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          image: require("../assets/Noticia2.jpg")
+          title: "News 2",
+          content: "This is the content of news 2",
+          image: require("../assets/Noticia2.jpg"),
+          comments: [],
+          likes: 0
         },
         {
           id: 3,
-          title: "Dummy News 3",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          image: "path/to/image3.jpg"
+          title: "News 3",
+          content: "This is the content of news 3",
+          image: require("../assets/Noticia3.jpg"),
+          comments: [],
+          likes: 0
         },
         {
           id: 4,
-          title: "Dummy News 4",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          image: "path/to/image4.jpg"
+          title: "News 4",
+          content: "This is the content of news 4",
+          image: require("../assets/Noticia4.jpg"),
+          comments: [],
+          likes: 0
         }
-      ]
+      ],
+      commentInput: ""
     };
+  },
+  methods: {
+    addComment(newsId) {
+      const news = this.newsList.find(news => news.id === newsId);
+      if (news) {
+        news.comments.push({ id: Date.now(), text: this.commentInput });
+        this.commentInput = "";
+      }
+    },
+    likeNews(newsId) {
+      const news = this.newsList.find(news => news.id === newsId);
+      if (news) {
+        news.likes++;
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
-.grid-container {
+.news-grid {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 20px;
 }
 
-.grid-item {
-  background-color: #f2f2f2;
-  padding: 20px;
-  border-radius: 10px;
+.news-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.comment-bar {
+  margin-top: 10px;
+}
+
+.comment-bar input {
+  width: 100%;
+  padding: 5px;
+  margin-right: 10px;
+}
+
+.comments {
+  margin-top: 10px;
+}
+
+.comment {
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin-bottom: 5px;
+}
+
+button {
+  margin-top: 10px;
 }
 
 .news-image {
-  width: 100%;
+  width: 400px;
   height: auto;
-  border-radius: 5px;
-}
-
-.news-title {
-  margin-top: 10px;
-  font-size: 18px;
-  font-weight: bold;
-}
-
-.news-description {
-  margin-top: 5px;
-  font-size: 14px;
+  margin-bottom: 50px;
 }
 </style>
