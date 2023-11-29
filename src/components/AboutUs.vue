ff6ae081dcdb5de4794296a641b7e266db814a92
Carlos Bernal <carlitos9913@gmail.com>
2023-11-29 02:09:09 -0700

    componente de about us

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="about-us">
+    <h2>Sobre Nosotras</h2>
+    <p>
+      Bienvenidas a TechGirls, una red social diseñada exclusivamente para chicas de 8 a 15 años que buscan apoyo, comunicación y explorar el emocionante mundo académico y tecnológico.
+    </p>
+    <p>
+      En TechGirls, nos esforzamos por crear un ambiente seguro y amigable donde las chicas de escasos recursos puedan conectarse, aprender y compartir experiencias. Queremos inspirar el interés en la tecnología y fomentar el empoderamiento a través del conocimiento.
+    </p>
+    <h3>Características Principales</h3>
+    <ul>
+      <li>Acceso a foros de discusión moderados</li>
+      <li>Publicación de información relacionada con temas académicos y tecnológicos</li>
+      <li>Comunidad de chicas con intereses similares</li>
+      <li>Experiencia de usuario diseñada pensando en chicas de 8 a 15 años</li>
+      <li>Acceso desde Chromebooks proporcionadas por el gobierno del Estado con internet ilimitado</li>
+    </ul>
+    <p>
+      Estamos comprometidas a proporcionar recursos educativos y un espacio de apoyo donde cada chica pueda brillar y desarrollar su potencial. Únete a TechGirls y sé parte de esta increíble comunidad.
+    </p>
+    <br>
+    <br>
+    <h2>Un proyecto hecho por: </h2>
+    <strong>Jorge Chavez</strong><br>
+    <strong>Carlos Ramirez</strong>
+  </div>
+</template>
+
+<style scoped>
+.about-us {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  border-top: 1px solid #ddd; /* Línea superior para actuar como un listón */
+}
+
+h2 {
+  color: #2c3e50;
+}
+
+h3 {
+  color: #3498db;
+}
+
+ul {
+  list-style-type: disc;
+  margin-left: 20px;
+  color: #555;
+}
+</style>
