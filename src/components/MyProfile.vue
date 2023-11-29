c655fb36ba60f64ac50d12aa36cc076d386c9fa0
Carlos Bernal <carlitos9913@gmail.com>
2023-11-29 02:10:57 -0700

    modulo para desplegar mi imagen de perfil

---
+++ src/components/MyProfile.vue
@@ -0,0 +1,26 @@
+<!-- ProfileLink.vue -->
+
+<template>
+  <router-link :to="{ name: 'editable-profile' }">
+    <img src="../assets/pikachu.png" alt="Imagen de Perfil" class="profile-image" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      profileImage: "URL_DE_TU_IMAGEN_DE_PERFIL",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.profile-image {
+  width: 50px; /* Ajusta el tamaño de acuerdo a tus necesidades */
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
