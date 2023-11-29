caa5c8d1bacc118fcbbe52bc8352c5e197ca3ba5
Carlos Bernal <carlitos9913@gmail.com>
2023-11-29 02:09:41 -0700

    cambios para los nuevos modulos, perfil de usuario, etc

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <nav>
-    <SearchList/>
-    <search-list/>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/ContactCard">Contact Card Test</router-link> |
-    <router-link to="/NuevaEntrada">Nueva publicación</router-link> |
-    <router-link to="/entradas">Publicaciónes</router-link> |
-    <router-link to="/Chat">Chat</router-link> |
-    <router-link to="/amigos">Amigos</router-link> |
-    <router-link to="/followers">Seguidores</router-link> |
-    <router-link to="/login">Iniciar sesion</router-link> |
-  </nav>
-  <router-view/>
   <div>
-    <SearchList />
+    <nav>
+      <SearchList/>
+      <router-link to="/">Home</router-link> |
+      <!--<router-link to="/ContactCard">Contact Card Test</router-link> | -->
+      <router-link to="/NuevaEntrada">Nueva publicación</router-link> |
+      <router-link to="/entradas">Publicaciónes</router-link> |
+      <router-link to="/Chat">Chat</router-link> |
+      <router-link to="/amigos">Amigos</router-link> |
+      <router-link to="/followers">Seguidores</router-link> |
+      <router-link to="/login">Iniciar sesion</router-link> |
+      <MyProfile/>
+    </nav>
+    <router-view/>
+    <PrivacyPopup v-if="showPrivacyPopup" @close="closePrivacyPopup" />
+    <div>
+      <router-link to="/about">Sobre nosotros</router-link>
+    </div>
   </div>
 </template>
 
@@ -40,10 +43,25 @@ nav a.router-link-exact-active {
 }
 </style>
 <script>
-import searchList from "@/views/SearchList.vue";
+import PrivacyPopup from "@/components/PrivacyPopup.vue";
+import SearchList from "@/views/SearchList.vue";
+import MyProfile from "@/components/MyProfile.vue";
+
 export default {
-  components:{
-    searchList,
-  }
-}
+  components: {
+    MyProfile,
+    PrivacyPopup,
+    SearchList,
+  },
+  data() {
+    return {
+      showPrivacyPopup: true, // Controla la visibilidad del popup
+    };
+  },
+  methods: {
+    closePrivacyPopup() {
+      this.showPrivacyPopup = false;
+    },
+  },
+};
 </script>
