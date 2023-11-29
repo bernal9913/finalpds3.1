f67a271a36de564fc1dcad3df9ea22d9461df5f7
Carlos Bernal <78837548+bernal9913@users.noreply.github.com>
2023-11-28 19:44:27 -0700

    perfil de usuario con mas usuarios locales

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -31,6 +31,27 @@ const users = [
     birthday:"2 de diciembre de 1999",
     photo: "https://via.placeholder/150",
     location: "Monterrey"
+  },
+  {
+    id: 4,
+    name: "Kanye West",
+    birthday:"8 de junio de 1977",
+    photo: "https://via.placeholder/150",
+    location: "Hawaii"
+  },
+  {
+    id: 5,
+    name: "jungkook",
+    birthday:"1 de septiembre de 1997",
+    photo: "https://via.placeholder/150",
+    location: "En alguna parte de corea del sur"
+  },
+  {
+    id: 6,
+    name: "Chuu",
+    birthday:"20 de octubre de 1999",
+    photo: "https://via.placeholder/150",
+    location: "En alguna parte de corea del sur"
   }
 ]
 export default {
